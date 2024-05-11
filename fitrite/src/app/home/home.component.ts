042022069e5014add12ffa2ea609dc6c7a85aeaf
Carolyn Fenwick <carolyn@irondatabase.com>
2024-05-11 13:22:31 -0300

    rename directory

--- fitrite/src/app/home/home.component.ts
+++ fitrite/src/app/home/home.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [CommonModule,CarouselModule],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss',
+    selector: 'app-home',
+    standalone: true,
+    imports: [CommonModule, CarouselModule],
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-  images = ['assets/beach_deck_slider.jpg', 'assets/combo_slider.jpg',
-          'assets/double_deck_slider.jpg'];
+    images = ['assets/beach_deck_slider.jpg', 'assets/combo_slider.jpg',
+        'assets/double_deck_slider.jpg'];
 }
--- fitrite/src/app/our-work/our-work.component.ts
+++ fitrite/src/app/our-work/our-work.component.ts
@@ -2,23 +2,23 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-our-work',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './our-work.component.html',
-  styleUrl: './our-work.component.scss'
+    selector: 'app-our-work',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './our-work.component.html',
+    styleUrl: './our-work.component.scss'
 })
 export class OurWorkComponent {
-    deckImages = ['assets/images/gallery-2/beach_deck_1.jpg'];
+    deckImages = ['assets/images/deck-gallery/beach_deck_1.jpg'];
     kitchenImages = [];
     otherImages = [];
 
     constructor() { }
 
     getThumbnail(image: string): string {
-      const base = image.substring(0, image.lastIndexOf('.'));
-      const extension = image.substring(image.lastIndexOf('.'));
-      return `${base}_thumb${extension}`;
-  }
+        const base = image.substring(0, image.lastIndexOf('.'));
+        const extension = image.substring(image.lastIndexOf('.'));
+        return `${base}_thumb${extension}`;
+    }
 
 }
