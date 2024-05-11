4bbfe849337d417b7338281d88b5dae481964f2c
cjfenwick <carolyn@irondatabase.com>
2024-05-11 15:45:41 -0300

    lightbox in progress

--- fitrite/src/app/our-work/our-work.component.ts
+++ fitrite/src/app/our-work/our-work.component.ts
@@ -1,23 +1,54 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { IAlbum, Lightbox, LightboxModule } from 'ngx-lightbox';
 
 @Component({
     selector: 'app-our-work',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, HttpClientModule, LightboxModule],
+    providers: [HttpClient, Lightbox],
     templateUrl: './our-work.component.html',
     styleUrl: './our-work.component.scss',
 })
 export class OurWorkComponent {
-    deckImages = ['assets/images/deck-gallery/beach_deck_1.jpg'];
     kitchenImages = [];
     otherImages = [];
+    deckImages: Array<IAlbum> = []; // Initialize deckImages as an empty array of type string[]
 
-    constructor() {}
+    constructor(
+        private http: HttpClient,
+        private _lightbox: Lightbox
+    ) {
+
+        this.getImages('assets/images/deck-gallery').subscribe(images => {
+            images.forEach(image =>
+                this.deckImages.push({ src: image, thumb: this.getThumbnail(image) }));
+        });
+
+
+    }
 
     getThumbnail(image: string): string {
         const base = image.substring(0, image.lastIndexOf('.'));
         const extension = image.substring(image.lastIndexOf('.'));
         return `${base}_thumb${extension}`;
+    }
+
+    getImages(directory: string): Observable<string[]> {
+        const imageFile = `${directory}/images.json`;
+        return this.http.get<string[]>(imageFile).pipe(
+            map((images: string[]) => images.map((image: string) => `${directory}/${image}`))
+        );
+    }
+
+    open(index: number): void { // open lightbox
+        this._lightbox.open(this.deckImages, index);
+    }
+
+    close(): void { // close lightbox programmatically
+        this._lightbox.close();
     }
 }
