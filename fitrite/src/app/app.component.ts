84321572368ae1e47c04aaef2f982607b16c5e30
cjfenwick <carolyn@irondatabase.com>
2024-05-12 21:24:29 -0300

    home page mostly done

--- fitrite/src/app/app.component.ts
+++ fitrite/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { RouterOutlet, RouterLink,RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, CommonModule, RouterLink],
+    imports: [RouterOutlet, CommonModule, RouterLink, RouterLinkActive, CarouselModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
 })
--- fitrite/src/app/home/home.component.ts
+++ fitrite/src/app/home/home.component.ts
@@ -1,17 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CarouselModule } from 'ngx-bootstrap/carousel';
 @Component({
     selector: 'app-home',
     standalone: true,
-    imports: [CommonModule, CarouselModule],
+    imports: [CommonModule],
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-    images = [
-        'assets/beach_deck_slider.jpg',
-        'assets/combo_slider.jpg',
-        'assets/double_deck_slider.jpg',
-    ];
 }
